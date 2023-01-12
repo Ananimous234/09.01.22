547dd986a5c34e62cc8e12c764a70f8dc60d2858
Ananimous234 <119108999+Ananimous234@users.noreply.github.com>
2023-01-12 10:38:08 +0200

    а

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import colorama
-import inspect
-help(colorama)
-print(dir(colorama))
-print(inspect.ismodule(colorama))
+ЧАСТИНА 1
+DOG = 22;
+FROG = 4;
+CAT = 98;
+HORSE = 13;
+print(f"Dog index - {DOG}")
+print(f"Frog index - {FROG}")
+print(f"Cat index - {CAT}")
+print(f"Horse index - {HORSE}")
+
+
