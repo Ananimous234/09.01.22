baaaed514ef0c009bab03cfe3f1de9d2cb13964e
Ananimous234 <119108999+Ananimous234@users.noreply.github.com>
2023-01-12 11:16:08 +0200

    ДОМАШНЕ ЗАВДАННЯ 09.01.22

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-ЧАСТИНА 1
+"ЧАСТИНА 1"
 DOG = 22;
 FROG = 4;
 CAT = 98;
@@ -9,3 +9,36 @@ print(f"Cat index - {CAT}")
 print(f"Horse index - {HORSE}")
 
 
+"ЧАСТИНА 2"
+import math
+
+a = 61.7
+b = a
+print(math.ceil(b))
+c = 45
+x = False
+if c == 45:
+    x = True
+if c > 45:
+    x = False
+if c < 45:
+    x = False
+d = c
+print(f"Дійсність числа (c) = {x}")
+print(f"Число (c) = {d}")
+
+
+"ЧАСТИНА 3"
+a = 16.7
+b = 12.5
+P = (f"{(a+b)*2}")
+S = (f"{a*b}")
+print(f"P = {P}m, S = {S}m")
+
+
+"ЧАСТИНА 4"
+V = 80
+T = 0
+S1 = 200
+TF = (f'{S1/V}')
+print(f"Час дорівнює 200/80={TF}")
